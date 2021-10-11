9283ced5439ad27774a278cf34dc22493d7fa63c
pmvo <89677288+pmvo@users.noreply.github.com>
2021-10-11 12:59:59 -0500

    Problem 2 - Initial commit
    Problem 2- Part a
    Read dates from input, one date per line.  
    Each date's format must be as follows: March 1, 1990. Any date not following that format is incorrect and should be ignored. Use the find() method to parse the string and extract the date. The input ends with -1 on a line alone. Use the datetime module to get the current date. Ignore any dates that are later than the current date. Output each correct date as 3/1/1990.

---
+++ Homework2/Problem2.py
@@ -0,0 +1,53 @@
+#Name: Phat Vo
+#PSID: 2024127
+
+def compare(inputMonth,inputDay,inputYear): #This function is used to generate current system date, and compares it with the user input date
+    from datetime import date
+    today = date.today()
+    currentDay = today.day
+    currentMonth = today.month
+    currentYear = today.year
+#If the user input date is later than current date, return value 0 (Ignore). Or else return value 1
+    if currentYear < inputYear:
+        return 0
+    elif currentYear==inputYear and currentMonth<inputMonth:
+        return 0
+    elif currentYear==inputYear and currentMonth==inputMonth and currentDay<inputDay:
+        return 0
+    else:
+        return 1
+
+
+def convert_month(month): #This function is used to convert the month name into the month number
+    month_list = {'January':1, 'February':2, 'March':3, 'April':4, 'May':5, 'June':6, 'July':7, 'August':8, 'September':9, 'October':10, 'November':11, 'December':12 } #A dictionary to store the month name and the month number
+    if month in month_list:
+        return month_list[month]
+    else:
+        return -1
+
+str_input=''
+while (str_input!= '-1'): #A while loop is used to ask for user input, if the users enter -1, exit the loop
+    str_input = str(input())
+
+    if (str_input!='-1'):
+        split_str = str_input.split(" ")
+
+        if len(split_str)==3:
+            str_month=split_str[0]
+            str_day=split_str[1]
+            str_year=split_str[2]
+
+            if convert_month(str_month)!=-1:
+
+                if str_day[-1] ==',':
+                    int_day = int(str_day[:len(str_day) - 1])
+
+                    if  int_day<=31:
+                        int_month = (convert_month(str_month))
+
+                        if compare(int_month,int_day,int(str_year))==1:
+                            print(str(int_month)+'/'+str_day[:len(str_day) - 1]+'/'+str_year)
+
+
+
+
