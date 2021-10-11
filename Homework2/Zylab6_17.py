4d46bcf8ebd71ce2ee73fd93c843759389fb0ad3
pmvo <89677288+pmvo@users.noreply.github.com>
2021-10-11 12:48:02 -0500

    Add files via upload

---
+++ Homework2/Zylab6_17.py
@@ -0,0 +1,10 @@
+# Name: Phat Vo
+# ID: 2024127
+password = str(input())
+
+password =password.replace('i','!')
+password =password.replace('a','@')
+password =password.replace('m','M')
+password =password.replace('B','8')
+password =password.replace('o','.')
+print(password+'q*s')
